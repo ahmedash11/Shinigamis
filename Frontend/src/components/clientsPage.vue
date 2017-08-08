6054000468370e99788b293810b1a14c36d4c095
El-Etreby <omarezzatel-etreby@Omars-MacBook-Pro-7.local>
2017-08-08 03:53:17 +0200

    Client page updated

--- Frontend/src/components/clientsPage.vue
+++ Frontend/src/components/clientsPage.vue
@@ -7,16 +7,23 @@
       <header class="major">
         <h2>Clients</h2>
       </header>
-      <div class="box alt">
-        <div class="row uniform">
-          <section class=" 4u 6u(medium) 12u$(xsmall) " v-for="client in clients">
-            <img v-if="client.profileimg.path" :src="url+client.profileimg.path.replace('public','')">
-            <img v-else src="/static/images/pic07.jpg">
+      <section class="col-lg-12 col-md-12 col-sm-12 small-padding">
+        <div class="row" v-for="client in clients">
+          <div class="col-lg-4 col-md-4 col-sm-12">
+            <div data-animation class="team-member">
+              <img v-if="client.profileimg.path" :src="url+client.profileimg.path.replace('public','')">
+              <img v-else src="/static/images/pic07.jpg">
+            </div>
+          </div>
+          <div class="col-lg-8 col-md-6 col-sm-12">
             <h3>{{client.name}}</h3>
             <p>{{client.description}}</p>
-          </section>
+            <br>
+
+          </div>
+
         </div>
-      </div>
+      </section>
     </div>
   </section>
 
@@ -57,5 +64,18 @@ export default {
 .\34 u img {
   max-height: 100%;
   max-width: 100%;
+}
+
+img {
+  height: auto;
+}
+
+img {
+  max-width: 100%;
+  border: none;
+}
+
+@media (min-width: 768px) .col-sm-12 {
+  width: 100%;
 }
 </style>
