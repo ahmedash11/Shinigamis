ce78f8977f59d79cae735a0f2be5a75ab3be8273
El-Etreby <omarezzatel-etreby@Omars-MacBook-Pro-7.local>
2017-09-03 16:45:47 +0200

    make it better

--- Backend/config/database.js
+++ Backend/config/database.js
@@ -1,4 +1,12 @@
 module.exports = {
-  database: 'mongodb://localhost:27017/shinigamis',
+  database: 'mongodb://rashiedmaritimeservices:rashiedmaritime10@ds121534.mlab.com:21534/shinigamis',
   secret: 'yoursecret'
 }
+
+/*
+database: 'mongodb://localhost:27017/shinigamis',
+secret: 'yoursecret'
+
+database: 'mongodb://rashiedmaritimeservices:rashiedmaritime10@ds121534.mlab.com:21534/shinigamis',
+secret: 'yoursecret'
+*/
